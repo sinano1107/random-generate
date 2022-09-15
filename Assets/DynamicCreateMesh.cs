11606dc0379f4531ef9cfe1301a4d0b7f9c1bbcc
sinano1107 <tftf161107@icloud.com>
2022-09-15 17:16:30 +0900

    [update] スペースを押している間、三角形が震える

--- Assets/DynamicCreateMesh.cs
+++ Assets/DynamicCreateMesh.cs
@@ -4,30 +4,46 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class DynamicCreateMesh : MonoBehaviour
 {
-    [SerializeField] Material _colorMaterial;
+    private Mesh myMesh;
+    private Vector3[] myVertices;
+    private int[] myTriangles;
 
     private void Start()
     {
         // メッシュの作成
-        var mesh = new Mesh();
+        myMesh = new Mesh();
 
         // 頂点座標配列をメッシュにセット
-        mesh.SetVertices(new Vector3[]
-        {
+        myVertices = new Vector3[] {
             new Vector3(0, 1f),
             new Vector3(1f, -1f),
             new Vector3(-1f, -1f),
-        });
+        };
+        myMesh.SetVertices(myVertices);
 
         // インデックス配列をメッシュにセット
-        mesh.SetTriangles(new int[] { 0, 1, 2 }, 0);
+        myTriangles = new int[] { 0, 1, 2 };
+        myMesh.SetTriangles(myTriangles, 0);
 
         // MeshFilterを通してメッシュをMeshRendererにセット
         var filter = GetComponent<MeshFilter>();
-        filter.sharedMesh = mesh;
+        filter.sharedMesh = myMesh;
 
         // MeshRendererにMaterialをセット
         var meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.material = new Material(Shader.Find("Unlit/ColorShader"));
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) //スペースキーの入力
+        {
+            for (int i = 0; i < myVertices.Length; i++)
+            {
+                //頂点をずらす
+                myVertices[i] += new Vector3(Random.Range(-0.05f, 0.05f), Random.Range(-0.05f, 0.05f));
+            }
+            myMesh.SetVertices(myVertices); //新しい頂点を割り当てる
+        }
     }
 }
