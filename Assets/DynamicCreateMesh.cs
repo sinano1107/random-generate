33e21794f6fac9d3286998e3f50015f4d29680ca
sinano1107 <tftf161107@icloud.com>
2022-09-16 00:05:26 +0900

    [update] 最低限の増殖機能を追加

--- Assets/DynamicCreateMesh.cs
+++ Assets/DynamicCreateMesh.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
@@ -5,8 +6,8 @@ using UnityEngine;
 public class DynamicCreateMesh : MonoBehaviour
 {
     private Mesh myMesh;
-    private Vector3[] myVertices;
-    private int[] myTriangles;
+    private List<Vector3> myVertices;
+    private List<int> myTriangles;
 
     private void Start()
     {
@@ -24,16 +25,20 @@ public class DynamicCreateMesh : MonoBehaviour
         meshRenderer.material = new Material(Shader.Find("Unlit/ColorShader"));
     }
 
+    // 生成
     public void Generate()
     {
         const float min = -2.0f;
         const float max = 2.0f;
 
+        // リセット
+        myMesh.SetTriangles(new int[] { }, 0);
+
         // 頂点座標配列をメッシュにセット
-        myVertices = new Vector3[]
+        myVertices = new List<Vector3>()
         {
-            new Vector3(0, -1),
-            new Vector3(0, 1),
+            new Vector3(Random.Range(min, max), Random.Range(min, max)),
+            new Vector3(Random.Range(min, max), Random.Range(min, max)),
             new Vector3(Random.Range(min, max), Random.Range(min, max)),
         };
         myMesh.SetVertices(myVertices);
@@ -48,7 +53,38 @@ public class DynamicCreateMesh : MonoBehaviour
         var Z = a.x * b.y - b.x * a.y;
 
         // 点2が左側の時は0,2,1の順、右側の時は0,1,2の順で結ぶ
-        myTriangles = (Z > 0) ? new int[] { 0, 2, 1 } : new int[] { 0, 1, 2 };
+        myTriangles = (Z > 0) ? new List<int> { 0, 2, 1 } : new List<int> { 0, 1, 2 };
+        myMesh.SetTriangles(myTriangles, 0);
+    }
+
+    // 増殖
+    public void Proliferate()
+    {
+        const float range = 3.0f;
+
+        var vertex_0_index = Random.Range(0, 3);
+        var vertex_1_index = Random.Range(0, 3);
+        // 二点が被らなくなるまでシャッフル
+        while (vertex_0_index == vertex_1_index)
+        {
+            vertex_1_index = Random.Range(0, 3);
+        }
+
+        var vertex_0 = myVertices[vertex_0_index];
+        var vertex_1 = myVertices[vertex_1_index];
+
+        var x_max = System.Math.Max(vertex_0.x, vertex_1.x) + range;
+        var x_min = System.Math.Min(vertex_0.x, vertex_1.x) - range;
+        var y_max = System.Math.Max(vertex_0.y, vertex_1.y) + range;
+        var y_min = System.Math.Min(vertex_0.y, vertex_1.y) - range;
+
+        var vertex_2 = new Vector3(Random.Range(x_max, x_min), Random.Range(y_max, y_min));
+
+        myVertices.Add(vertex_2);
+        myMesh.SetVertices(myVertices);
+
+        var newTriangles = new List<int> { vertex_0_index, vertex_1_index, 3 };
+        myTriangles.AddRange(newTriangles);
         myMesh.SetTriangles(myTriangles, 0);
     }
 }
