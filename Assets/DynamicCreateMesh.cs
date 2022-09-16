b52d2a88650c7e506a6210bb5f315701df9ade6e
sinano1107 <tftf161107@icloud.com>
2022-09-16 16:28:32 +0900

    [update] 三角形の内心を表示

--- Assets/DynamicCreateMesh.cs
+++ Assets/DynamicCreateMesh.cs
@@ -8,6 +8,7 @@ public class DynamicCreateMesh : MonoBehaviour
     private Mesh myMesh;
     private List<Vector3> myVertices;
     private List<int> myTriangles;
+    private Vector3 myTriangleCenter;
 
     private void Start()
     {
@@ -55,6 +56,10 @@ public class DynamicCreateMesh : MonoBehaviour
         // 点2が左側の時は0,2,1の順、右側の時は0,1,2の順で結ぶ
         myTriangles = (Z > 0) ? new List<int> { 0, 2, 1 } : new List<int> { 0, 1, 2 };
         myMesh.SetTriangles(myTriangles, 0);
+
+        // 三角形の内心を保存
+        myTriangleCenter = GetTriangleCenter(myVertices[0], myVertices[1], myVertices[2]);
+        GameObject.Find("TriangleCenter").transform.position = myTriangleCenter;
     }
 
     // 増殖
@@ -73,6 +78,7 @@ public class DynamicCreateMesh : MonoBehaviour
         var vertex_0 = myVertices[vertex_0_index];
         var vertex_1 = myVertices[vertex_1_index];
 
+        // vertex_2の範囲を設定
         var x_max = System.Math.Max(vertex_0.x, vertex_1.x) + range;
         var x_min = System.Math.Min(vertex_0.x, vertex_1.x) - range;
         var y_max = System.Math.Max(vertex_0.y, vertex_1.y) + range;
@@ -80,11 +86,26 @@ public class DynamicCreateMesh : MonoBehaviour
 
         var vertex_2 = new Vector3(Random.Range(x_max, x_min), Random.Range(y_max, y_min));
 
+        // todo vertex_2が折り返る形になっていた時、再試行する
+
         myVertices.Add(vertex_2);
         myMesh.SetVertices(myVertices);
 
         var newTriangles = new List<int> { vertex_0_index, vertex_1_index, 3 };
         myTriangles.AddRange(newTriangles);
         myMesh.SetTriangles(myTriangles, 0);
+    }
+
+    // 三角形の内心の座標を求める
+    private Vector3 GetTriangleCenter(Vector3 A, Vector3 B, Vector3 C)
+    {
+        var a = Vector3.Distance(B, C);
+        var b = Vector3.Distance(C, A);
+        var c = Vector3.Distance(A, B);
+
+        var x = (a * A.x + b * B.x + c * C.x) / (a + b + c);
+        var y = (a * A.y + b * B.y + c * C.y) / (a + b + c);
+
+        return new Vector3(x, y);
     }
 }
