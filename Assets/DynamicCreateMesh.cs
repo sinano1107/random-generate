776c5fb181f7f2750b46df3b60df2004c3b3b5a6
sinano1107 <tftf161107@icloud.com>
2022-09-25 06:15:20 +0900

    [update] 三角形の内心点がになっていた役割をを辺に選択されていない頂点へ変更

--- Assets/DynamicCreateMesh.cs
+++ Assets/DynamicCreateMesh.cs
@@ -9,7 +9,6 @@ public class DynamicCreateMesh : MonoBehaviour
     private Mesh myMesh;
     private List<Vector3> myVertices;
     private List<int> myTriangles;
-    private Vector3 myTriangleCenter;
     private List<(Vector3 X, Vector3 Y)> mySides;
 
     private void Start()
@@ -52,10 +51,6 @@ public class DynamicCreateMesh : MonoBehaviour
         myTriangles = (Z > 0) ? new List<int> { 0, 2, 1 } : new List<int> { 0, 1, 2 };
         // myMesh.SetTriangles(myTriangles, 0);
 
-        // 三角形の内心を保存
-        myTriangleCenter = GetTriangleCenter(myVertices[0], myVertices[1], myVertices[2]);
-        GameObject.Find("TriangleCenter").transform.position = myTriangleCenter;
-
         // 辺を保存
         mySides = new List<(Vector3, Vector3)>
         {
@@ -73,6 +68,9 @@ public class DynamicCreateMesh : MonoBehaviour
         var choiceSideIndex = Random.Range(0, mySides.Count);
         var (vertex_0, vertex_1) = mySides[choiceSideIndex];
 
+        // 辺に含まれない頂点を保存
+        var ostracized = myVertices[3 - myVertices.IndexOf(vertex_0) - myVertices.IndexOf(vertex_1)];
+
         // vertex_2の範囲を設定
         var x_max = System.Math.Max(vertex_0.x, vertex_1.x) + range;
         var x_min = System.Math.Min(vertex_0.x, vertex_1.x) - range;
@@ -83,7 +81,7 @@ public class DynamicCreateMesh : MonoBehaviour
         var vertex_2 = new Vector3(Random.Range(x_max, x_min), Random.Range(y_max, y_min));
 
         // 核三角形の内心と、vertex_2のZ成分を算出
-        var triangleCenter_Z = GetZ(vertex_0, vertex_1, myTriangleCenter);
+        var triangleCenter_Z = GetZ(vertex_0, vertex_1, ostracized);
         var vertex_2_Z = GetZ(vertex_0, vertex_1, vertex_2);
 
         // triangleCenter_Zとvertex_2_Zの正負が等しい場合、
@@ -174,19 +172,6 @@ public class DynamicCreateMesh : MonoBehaviour
         mySides.RemoveAt(choiceSideIndex);
         mySides.Add((vertex_0, vertex_2));
         mySides.Add((vertex_1, vertex_2));
-    }
-
-    // 三角形の内心の座標を求める
-    private Vector3 GetTriangleCenter(Vector3 A, Vector3 B, Vector3 C)
-    {
-        var a = Vector3.Distance(B, C);
-        var b = Vector3.Distance(C, A);
-        var c = Vector3.Distance(A, B);
-
-        var x = (a * A.x + b * B.x + c * C.x) / (a + b + c);
-        var y = (a * A.y + b * B.y + c * C.y) / (a + b + c);
-
-        return new Vector3(x, y);
     }
 
     // 外積のZを計算 返り値が正なら左、負なら右
